6178600053299c01c58ec330497b8eb1c94bbec5
nexus0943 <48218016+nexus0943@users.noreply.github.com>
2021-09-28 17:34:04 +0300

    Sum

---
+++ Application/client/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import AppNavbar from '@/components/AppNavbar.vue';
+  import AppSideBar from '@/components/AppSideBar.vue';
+  
+  export default {
+    components: {
+      AppNavbar,
+      AppSideBar
+    },
+  };
+</script>
+
+<template>
+  <div class="sb-nav-fixed">
+    <AppNavbar />
+    <div id="layoutSidenav">
+     <AppSideBar /> 
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
